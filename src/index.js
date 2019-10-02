1a3c8ad161881040d767d0cd06f421386f8fa22a
Sinisa Milicic <shasini92@gmail.com>
2019-10-02 23:08:24 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import {Doktor} from "./Doktor";
+import {Pacijent} from "./Pacijent";
+// import { KrvniPritisak } from "KrvniPritisak";
+// import { NivoHolesterola } from "NivoHolesterola";
+// import { NivoSecera } from "NivoSecera";
+
+let Milan = new Doktor("Milan111", "Ilic", "Interna Medicina");
+let Dragan = new Pacijent("Dragan", "Petrovic", 1309992182813, 145);
+
+function main() {
+  console.log('Retard');
+  console.log(Dragan);
+  console.log('srbija');
+}
+
+main();
